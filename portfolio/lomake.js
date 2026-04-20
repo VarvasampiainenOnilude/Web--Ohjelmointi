a45331298706dd75b3a3b9aecbd58f3ede3c5d8f
Oluwatobi Onilude <oo3452@edu.turku.fi>
2026-04-20 13:47:04 +0300

    Muuttuja tehtävän fixaus ja JS tietotyypit valmiiksi!

--- portfolio/lomake.js
+++ portfolio/lomake.js
@@ -1,1 +1,17 @@
-console.log("Lomake-sivu avattu");
+function send(e) {
+    e.preventDefault(); // estää sivun uudelleenlatauksen
+
+    let birthday = document.getElementById("birthday").value;
+    let email = document.getElementById("email").value;
+    let type = document.getElementById("type").value;
+    let usage = document.getElementById("usage").checked;
+    let body = document.getElementById("body").value;
+
+    console.log(birthday);
+    console.log(email);
+    console.log(type);
+    console.log(usage);
+    console.log(body);
+
+    document.getElementById("moi").close();
+}
--- portfolio/muuttuja.js
+++ portfolio/muuttuja.js
@@ -1,21 +1,39 @@
-let a = 3;
-let b = 5;
-let c = 7;
-let d = 15;
-
-
-function summa() {
-    alert(a + b);
+function summa(x, y) {
+    alert(x + y);
 }
 
-function vahennys() {
-    alert(b - c);
+function vahennys(x, y) {
+    alert(x - y);
 }
 
-function kerto() {
-    alert(c * d);
+function kerto(x, y) {
+    alert(x * y);
 }
 
-function jako() {
-    alert(d / a);
+function jako(x, y) {
+    alert(x / y);
+}
+
+function laskin_summa() {
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    summa(eka, toka);
+}
+
+function laskin_vahennys() {
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    vahennys(eka, toka);
+}
+
+function laskin_kerto() {
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    kerto(eka, toka);
+}
+
+function laskin_jako() {
+    let eka = Number(document.getElementById("numero1").value);
+    let toka = Number(document.getElementById("numero2").value);
+    jako(eka, toka);
 }
