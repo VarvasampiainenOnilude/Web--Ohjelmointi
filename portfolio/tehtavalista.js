b6f5b2fdcf5004e9402cabab9cd7fe63e1caf702
Oluwatobi Onilude <oo3452@edu.turku.fi>
2026-05-07 13:19:16 +0300

    tehtavalista tehty

--- portfolio/tehtavalista.js
+++ portfolio/tehtavalista.js
@@ -4,21 +4,27 @@ function paivitaLista() {
     let ul = document.getElementById("lista");
     ul.innerHTML = "";
 
-    for (let i = 0; i < lista.length; i++) {
+    for (let arvo of lista) {
         let li = document.createElement("li");
-        li.innerHTML = lista[i];
+        li.innerHTML = arvo;
         ul.appendChild(li);
     }
 }
 
 function lisaa() {
-    let input = document.getElementById("uusi");
-    let arvo = input.value;
+    let input = document.getElementById("input");
+    let maaraInput = document.getElementById("maara");
 
-    if (arvo.trim() !== "") {
-        lista.push(arvo);
+    let arvo = input.value.trim();
+    let maara = Number(maaraInput.value);
+
+    if (arvo !== "" && maara > 0) {
+        for (let i = 0; i < maara; i++) {
+            lista.push(arvo);
+        }
         paivitaLista();
         input.value = "";
+        maaraInput.value = 1;
     }
 }
 
