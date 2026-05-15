8e816d9bb8056f6dcf43481738bb8a2d459b7adf
Oluwatobi.onilude <oo3452@edu.turku.fi>
2026-05-15 04:04:21 +0300

    paivamäärä tehty!

--- portfolio/muuttuja.js
+++ portfolio/muuttuja.js
@@ -37,3 +37,61 @@ function laskin_jako() {
     let toka = Number(document.getElementById("numero2").value);
     jako(eka, toka);
 }
+
+function naytaPaivamaara() {
+
+  let nyt = new Date();
+
+  let paiva = nyt.getDate();
+  let kuukausiNumero = nyt.getMonth();
+  let vuosi = nyt.getFullYear();
+
+  let tunnit = nyt.getHours();
+  let minuutit = nyt.getMinutes();
+  let sekunnit = nyt.getSeconds();
+
+  let viikonpaivat = [
+    "sunnuntai",
+    "maanantai",
+    "tiistai",
+    "keskiviikko",
+    "torstai",
+    "perjantai",
+    "lauantai"
+  ];
+
+  let kuukaudet = [
+    "tammikuu",
+    "helmikuu",
+    "maaliskuu",
+    "huhtikuu",
+    "toukokuu",
+    "kesäkuu",
+    "heinäkuu",
+    "elokuu",
+    "syyskuu",
+    "lokakuu",
+    "marraskuu",
+    "joulukuu"
+  ];
+
+  let viikonpaiva = viikonpaivat[nyt.getDay()];
+  let kuukausi = kuukaudet[kuukausiNumero];
+
+  document.getElementById("paivamaara").innerHTML =
+    paiva + "." + (kuukausiNumero + 1) + "." + vuosi;
+
+  document.getElementById("aika").innerHTML =
+    tunnit + "." + minuutit + "." + sekunnit;
+
+  document.getElementById("paivaJaAika").innerHTML =
+    paiva + "." + (kuukausiNumero + 1) + "." + vuosi +
+    " klo " +
+    tunnit + "." + minuutit + "." + sekunnit;
+
+  document.getElementById("viikonpaiva").innerHTML =
+    viikonpaiva;
+
+  document.getElementById("kuukausi").innerHTML =
+    kuukausi;
+}
